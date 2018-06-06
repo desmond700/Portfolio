bd92add8afd8ce8eb536ed3cc45c9ae548a83a60
desmond700 <dwdeveloper4@gmail.com>
2018-06-05 21:14:47 -0400

    removed

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php include("include/header.php") ?>
+  <section class="d-flex landing-page">
+        <div class="my-auto mx-auto d-flex justify-content-center col-md-12">
+          <div class="text-white mx-auto">
+            <h2>Front end and Backend Web Developer</h2>
+            <p>Coding is my passion.</p>
+            <div class="text-center">
+              <img class="rounded-circle" width="200" height="200" src="https://images.pexels.com/photos/462680/pexels-photo-462680.jpeg?cs=srgb&dl=beard-blur-casual-462680.jpg&fm=jpg" alt="">
+            </div>
+          </div>
+        </div>
+
+    </section>
+    <section class="about pb-5" id="About">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 py-4">
+            <h2 class="text-center">About Me</h2>
+            <hr>
+          </div>
+          <div class="col-md-4">
+            <h2>About Me</h2>
+            <address class="">
+              <p>Name: Desmond Wallace</p>
+              <p>Age: 25 years old</p>
+              <p>City: Brampton, ON</p>
+              <p>Phone#: 647-864-8714</p>
+              <p>Email: <a href="mailto:dwdeveloper4@gmail.com">dwdeveloper4@gmail.com</a></p>
+            </address>
+          </div>
+          <div class="col-md-4">
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quod quasi mollitia neque, officia commodi nobis cumque. Veritatis, ut, sint!</p>
+          </div>
+          <div class="col-md-4">
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ex, nulla atque dolorem recusandae placeat molestias perspiciatis quaerat magni sapiente.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="skills pb-5" id="Skills">
+      <div class="container">
+        <div class="col-md-12 py-4">
+          <h2 class="text-center">Skills</h2>
+          <hr>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>HTML & CSS &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Bootstrap &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>JavaScript &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>PHP & MSQL &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Java &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>C# &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Java &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-12 mb-5">
+          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Java &nbsp;</h5> <h5>Intermediate</h5></div>
+          <div class="flex-grow-1 progress my-auto">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
+            aria-valuemin="0" aria-valuemax="100" style="width:40%">
+              40% Complete (success)
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div id="feed"></div>
+      </div>
+    </section>
+
+<?php include("include/footer.php") ?>
