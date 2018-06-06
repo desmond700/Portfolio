54d958ad91e9bf237d2208011eed2bb1bc07b700
desmond700 <dwdeveloper4@gmail.com>
2018-06-06 12:05:39 -0400

    Removed unwanted codes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("include/header.php") ?>
   <section class="d-flex landing-page">
         <div class="my-auto mx-auto d-flex justify-content-center col-md-12">
-          <div class="text-white mx-auto">
+          <div class="mx-auto">
             <h2>Front end and Backend Web Developer</h2>
             <p>Coding is my passion.</p>
             <div class="text-center">
@@ -39,86 +39,7 @@
         </div>
       </div>
     </section>
-    <section class="skills pb-5" id="Skills">
-      <div class="container">
-        <div class="col-md-12 py-4">
-          <h2 class="text-center">Skills</h2>
-          <hr>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>HTML & CSS &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Bootstrap &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>JavaScript &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>PHP & MSQL &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Java &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>C# &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Java &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-12 mb-5">
-          <div class="col-md-12 d-flex justify-content-between px-0"><h5>Java &nbsp;</h5> <h5>Intermediate</h5></div>
-          <div class="flex-grow-1 progress my-auto">
-            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40"
-            aria-valuemin="0" aria-valuemax="100" style="width:40%">
-              40% Complete (success)
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    
 
     <section>
       <div class="container">
