2ad2e438a7c25de5fe7255e1ff6784fe328df753
desmond700 <dwdeveloper4@gmail.com>
2018-06-07 12:10:55 -0400

    Added code for view page

---
+++ projects/view.php
@@ -0,0 +1,12 @@
+<?php include("../include/header.php") ?>
+<div class="container" style="margin-top:8em;margin-bottom: 5em">
+  <div class="container">
+    <div class="col-md-12 py-4">
+      <h2 class="text-center">Projects</h2>
+      <hr>
+    </div>
+    <div class="row">
+    </div>
+  </div>
+</div>
+<?php include("../include/footer.php") ?>
