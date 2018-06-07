4b045de34ca323b2117fbd271533a4224adc7bbb
desmond700 <dwdeveloper4@gmail.com>
2018-06-07 12:10:36 -0400

    Removed unnecessary codes

--- projects/index.php
+++ projects/index.php
@@ -1,51 +1,11 @@
 <?php include("../include/header.php") ?>
 <div class="container" style="margin-top:8em;margin-bottom: 5em">
   <div class="container">
-    <div class="row">
-      <div class="col-md-12 py-4">
-        <h2 class="text-center">Projects</h2>
-        <hr>
-      </div>
-      <div class="col-6 col-md-3 py-3">
-        <div class="card">
-          <img class="img-responsive" height="180" src="/Portfolio/images/thumbnails/BookStoreHome.PNG" alt="">
-          <div class="card-body">
-            <p class="card-text">A book store website which incorporates technologies such as service workers, web workers and indexeddb</p>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">View Details</button>
-            <a href="#" class="card-link">Another link</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 col-md-3 py-3">
-        <div class="card">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some example text. Some example text.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 col-md-3 py-3">
-        <div class="card">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some example text. Some example text.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 col-md-3 py-3">
-        <div class="card">
-          <img class="img-responsive" height="180" src="/Portfolio/images/thumbnails/manage_listcar.PNG" alt="">
-          <div class="card-body">
-            <p class="card-text">A car-manager app created using C# winform</p>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">View Details</button>
-            <a href="#" class="card-link">Another link</a>
-          </div>
-        </div>
-      </div>
+    <div class="col-md-12 py-4">
+      <h2 class="text-center">Projects</h2>
+      <hr>
+    </div>
+    <div class="row" id="projects">
     </div>
   </div>
 </div>
