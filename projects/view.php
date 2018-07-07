7e988053c0b3f33cde30f3e94089631111f81b89
desmond700 <dwdeveloper4@gmail.com>
2018-07-07 12:33:17 -0400

    Added breadcrumb and divs for screenshoots

--- projects/view.php
+++ projects/view.php
@@ -1,11 +1,26 @@
 <?php include("../include/header.php") ?>
 <div class="container" style="margin-top:8em;margin-bottom: 5em">
-  <div class="container">
-    <div class="col-md-12 py-4">
-      <h2 class="text-center">Projects</h2>
-      <hr>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="../">Home</a></li>
+      <li class="breadcrumb-item"><a href="./">Projects</a></li>
+      <li class="breadcrumb-item active name" aria-current="page"></li>
+    </ol>
+  </nav>
+  <div class="col-md-12 px-0">
+    <div class="title"></div>
+    <hr>
+  </div>
+  <div class="row">
+    <div class="col-md-12 py-2 my-2" id="info">
+
     </div>
-    <div class="row">
+    <div class="col-md-12">
+      <div class="container">
+        <div class="row" id="screenshots">
+
+        </div>
+      </div>
     </div>
   </div>
 </div>
