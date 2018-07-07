61dab9f2a7b4d742e3657d9df0e516355ae2c51b
desmond700 <dwdeveloper4@gmail.com>
2018-07-07 12:34:03 -0400

    Added breadcrumb

--- projects/index.php
+++ projects/index.php
@@ -1,12 +1,16 @@
 <?php include("../include/header.php") ?>
 <div class="container" style="margin-top:8em;margin-bottom: 5em">
-  <div class="container">
-    <div class="col-md-12 py-4">
-      <h2 class="text-center">Projects</h2>
-      <hr>
-    </div>
-    <div class="row" id="projects">
-    </div>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="../">Home</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Projects</li>
+    </ol>
+  </nav>
+  <div class="col-md-12 py-4">
+    <h2 class="text-center">Projects</h2>
+    <hr>
+  </div>
+  <div class="row" id="projects">
   </div>
 </div>
 <?php include("../include/footer.php") ?>
